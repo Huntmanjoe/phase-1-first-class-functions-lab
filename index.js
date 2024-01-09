3cbed7fb4722322bd05e355ef1361697d1c4393e
Hunter <Huntermatyi@gmail.com>
2024-01-09 11:45:08 -0500

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function(fare) { 
+        return fare * integer;
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
